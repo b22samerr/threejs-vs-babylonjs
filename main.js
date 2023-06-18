32855276ccac450243fb4606c9cb8857976fa7f9
Björn Hjorth <hjorth.bjorn@gmail.com>
2023-06-18 21:58:55 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,51 @@
+import { CreateSphere } from "@babylonjs/core/Meshes/Builders/sphereBuilder.js"
+import { CreateGround } from "@babylonjs/core/Meshes/Builders/groundBuilder.js"
+import { HemisphericLight } from "@babylonjs/core/Lights/hemisphericLight.js"
+import { FreeCamera } from "@babylonjs/core/Cameras/freeCamera.js"
+import { Vector3 } from "@babylonjs/core/Maths/math.vector.js"
+import { Engine } from "@babylonjs/core/Engines/engine.js"
+import { Scene } from "@babylonjs/core/scene.js"
+
+// If you don't need the standard material you will still need to import it since the scene requires it.
+// @ts-ignore
+import { StandardMaterial } from "@babylonjs/core/Materials/standardMaterial.js";
+
+/** @type {HTMLCanvasElement} */
+const canvas = document.querySelector("#game-canvas")
+
+const engine = new Engine(canvas, true)
+
+// This creates a basic Babylon Scene object (non-mesh)
+const scene = new Scene(engine)
+
+// This creates and positions a free camera (non-mesh)
+const camera = new FreeCamera("camera1", new Vector3(0, 5, -10), scene)
+
+// This targets the camera to scene origin
+camera.setTarget(Vector3.Zero())
+
+// This attaches the camera to the canvas
+camera.attachControl(canvas, true)
+
+// This creates a light, aiming 0,1,0 - to the sky (non-mesh)
+const light = new HemisphericLight("light", new Vector3(0, 1, 0), scene)
+
+// Default intensity is 1. Let's dim the light a small amount
+light.intensity = 0.7
+
+// Our built-in 'sphere' shape.
+const sphere = CreateSphere("sphere", { diameter: 2, segments: 32 }, scene)
+
+// Move the sphere upward 1/2 its height
+sphere.position.y = 1
+
+// Our built-in 'ground' shape.
+CreateGround("ground", { width: 6, height: 6}, scene)
+
+window.addEventListener("resize", () => {
+    engine.resize()
+})
+
+engine.runRenderLoop(() => {
+    scene.render()
+})
