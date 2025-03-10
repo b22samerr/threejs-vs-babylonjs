b641935353108bd8143eec1f1a8973e0f44f14d5
Samue <b22samer@student.his.se>
2025-03-10 20:27:06 +0100

    Implementerar kod som skapar ett säte, detta säte kan potentiellt användas för att skapa en stol

--- main.js
+++ main.js
@@ -1,3 +1,34 @@
 import './style.css';
 import * as THREE from 'three';
 
+const scene = new THREE.Scene();
+
+const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000);
+
+const renderer = new THREE.WebGLRenderer({
+  canvas: document.querySelector('#bg'),
+})
+
+renderer.setPixelRatio( window.devicePixelRatio );
+renderer.setSize( window.innerWidth, window.innerHeight );
+camera.position.set(0, 10, 20);
+camera.lookAt(0, 5, 0);
+
+const light = new THREE.DirectionalLight(0xffffff, 1);
+light.position.set(5, 10, 5);
+scene.add(light);
+
+const material = new THREE.MeshStandardMaterial({ color: 0x8B4513 });
+
+//Stolsits
+const seatGeometry = new THREE.BoxGeometry(6, 1, 6);
+const seat = new THREE.Mesh(seatGeometry, material);
+seat.position.set(0, 5, 0);
+scene.add(seat);
+
+function animate() {
+  requestAnimationFrame( animate );
+  renderer.render( scene, camera );
+}
+
+animate()
