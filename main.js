761f429b0c3e953df4819682d307bc1f6bd3f3b9
Samue <b22samer@student.his.se>
2025-03-30 15:41:25 +0200

    Implementerar kod som skapar ett säte, detta säte kan potentiellt användas för att skapa en stol

--- main.js
+++ main.js
@@ -1,26 +1,29 @@
-import { FreeCamera, Engine, Scene, Vector3, HemisphericLight, CreateSphere, CreateGround } from "@babylonjs/core"
+import { FreeCamera, Engine, Scene, Vector3, HemisphericLight, MeshBuilder, StandardMaterial, Color3, Color4 } from "@babylonjs/core";
 
 
 // If you don't need the standard material you will still need to import it since the scene requires it.
 // @ts-ignore
-import { StandardMaterial } from "@babylonjs/core/Materials/standardMaterial.js";
 
 /** @type {HTMLCanvasElement} */
+// @ts-ignore
 const canvas = document.querySelector("#babylon-canvas")
 
 const engine = new Engine(canvas, true)
 
 // This creates a basic Babylon Scene object (non-mesh)
-const scene = new Scene(engine)
+const scene = new Scene(engine);
 
 // This creates and positions a free camera (non-mesh)
-const camera = new FreeCamera("camera1", new Vector3(0, 5, -10), scene)
+const camera = new FreeCamera("camera1", new Vector3(-20, 10, 0), scene)
 
 // This targets the camera to scene origin
 camera.setTarget(Vector3.Zero())
 
 // This attaches the camera to the canvas
 camera.attachControl(canvas, true)
+
+
+scene.clearColor = new Color4(0, 0, 0, 1)
 
 // This creates a light, aiming 0,1,0 - to the sky (non-mesh)
 const light = new HemisphericLight("light", new Vector3(0, 1, 0), scene)
@@ -29,13 +32,20 @@ const light = new HemisphericLight("light", new Vector3(0, 1, 0), scene)
 light.intensity = 0.7
 
 // Our built-in 'sphere' shape.
-const sphere = CreateSphere("sphere", { diameter: 2, segments: 32 }, scene)
+const seat = MeshBuilder.CreateBox("seat", {
+    width: 5,
+    height: 5,
+    depth: 1,
+}, scene);
 
-// Move the sphere upward 1/2 its height
-sphere.position.y = 1
+seat.position = new Vector3(0,0,0);
 
-// Our built-in 'ground' shape.
-CreateGround("ground", { width: 6, height: 6}, scene)
+const material = new StandardMaterial("seatMaterial", scene);
+material.diffuseColor = new Color3(0.7, 0.8, 0.8);
+seat.material = material;
+
+
+seat.rotation = new Vector3(Math.PI / 2, 0, 0);
 
 window.addEventListener("resize", () => {
     engine.resize()
