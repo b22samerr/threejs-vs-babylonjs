72d52c935fb38783f675a94e6874feb6e286b2f5
Samue <b22samer@student.his.se>
2025-05-09 15:12:35 +0200

    Implementering av Sfärer.
    Implementerar kod för att skapa en sfär och ändra ljuset så att denna syns korrekt.
    
    Koden som används efterliknar strukturen som används för att rendera samma figurer genom Babylon.js

--- main.js
+++ main.js
@@ -4,6 +4,8 @@ import * as THREE from 'three';
 const scene = new THREE.Scene();
 
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000);
+camera.position.set(2, 10, -60);
+
 
 const renderer = new THREE.WebGLRenderer({
   canvas: document.querySelector('#bg'),
@@ -11,91 +13,48 @@ const renderer = new THREE.WebGLRenderer({
 
 renderer.setPixelRatio( window.devicePixelRatio );
 renderer.setSize( window.innerWidth, window.innerHeight );
-camera.position.set(0, 10, 20);
-camera.lookAt(0, 5, 0);
+camera.position.set(0, 0, -30);
+camera.lookAt(0, 0, 0);
 
 const light = new THREE.AmbientLight(0xffffff, 1);
 light.position.set(5, 10, 5);
 scene.add(light);
 
-const material = new THREE.MeshStandardMaterial({ color: 0x5D3317 });
+const material = new THREE.MeshStandardMaterial({ 
+  color: 0x5D3317,
+  opacity: 1, 
+});
 
-function chair(x,y,z) {
-  const group = new THREE.Group();
-
-  //Stolsits
-  const seatGeometry = new THREE.BoxGeometry(3, 0.5, 3);
-  const seat = new THREE.Mesh(seatGeometry, material);
-  seat.position.set(0, 2.5, 0);
-  group.add(seat);
-
-  //Ryggstöd
-  const backGeometry = new THREE.BoxGeometry(3, 3, 0.5);
-  const back = new THREE.Mesh(backGeometry, material);
-  back.position.set(0, 4, -1.25);
-  group.add(back);
-
-  //Stolsben Högerfram (hf)
-  const hfGeometry = new THREE.BoxGeometry(0.5, 2, 0.5);
-  const hf = new THREE.Mesh(hfGeometry, material);
-  hf.position.set(1.25, 1.25, 1.25);
-  group.add(hf);
-
-  //Stolsben Vänsterfram (vf)
-  const vfGeometry = new THREE.BoxGeometry(0.5, 2, 0.5);
-  const vf = new THREE.Mesh(vfGeometry, material);
-  vf.position.set(-1.25, 1.25, 1.25);
-  group.add(vf);
-
-  //Stolsben Högerbak (hb)
-  const hbGeometry = new THREE.BoxGeometry(0.5, 2, 0.5);
-  const hb = new THREE.Mesh(hbGeometry, material);
-  hb.position.set(1.25, 1.25, -1.25);
-  group.add(hb);
-
-  //Stolsben Vänsterbak (vb)
-  const vbGeometry = new THREE.BoxGeometry(0.5, 2, 0.5);
-  const vb = new THREE.Mesh(vbGeometry, material);
-  vb.position.set(-1.25, 1.25, -1.25);
-  group.add(vb);
-
-  group.position.set(x, y, z);
-  scene.add(group);
+function renderSphere(position) {
+  const geometry = new THREE.SphereGeometry(2, 32, 32);
+  const sphere = new THREE.Mesh(geometry, material);
+  sphere.position.copy(position);
+  scene.add(sphere);
 }
 
 
 //Iteration av rendering för  en enkel visualiserings scen
-function renderChairs() {
-  //Spara tid innan renderingen påbörjas
-  const startTime = performance.now();
 
-  let x = -30;
+function render() {
+
+  let x = -38;
   let y = -12;
 
   //Iteration för 48 figurer
   for(let i = 0; i < 48; i++){
-    chair(x,y,0);
+    renderSphere(new THREE.Vector3(x, y, 0));
 
-    x += 5;
+    x += 7;
     //När 12 figurer renderats ändras positioneringen på y-axeln, detta skapar en ny rad
     if((i + 1) % 12 === 0 ) {
-      x = -28;
+      x = -38;
       y += 8;
 
     }
   }
-
-  //Spara tiden efter att alla figurer renderats
-  requestAnimationFrame(() => {
-    const endTime = performance.now(); // Mäta efter att WebGL renderat första gången
-    let measure = "Renderingstid:";
-    localStorage.setItem(measure,endTime - startTime);
-  });
-
 }
 
-renderChairs();
-
+render();
 function animate() {
   requestAnimationFrame( animate );
   //scene.rotation.y += (0.01);
@@ -103,3 +62,9 @@ function animate() {
 }
 
 animate()
+
+window.addEventListener('resize', () => {
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+  renderer.setSize(window.innerWidth, window.innerHeight);
+});
